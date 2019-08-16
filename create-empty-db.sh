699662d8d06719deeb50b2caa721547c632667df
gozermon <gozerads@gmail.com>
2019-08-16 14:06:34 -0400

    Change System.out to log.info() in SuspectRunnable Update the Neo4j docker tag to 3.5-enterprise Change the container plugins directory to point to the neo4j sub-directory)

--- create-empty-db.sh
+++ create-empty-db.sh
@@ -4,18 +4,19 @@ docker stop neo4j-empty
 
 PASSWORD=admin
 CWD=`pwd`
-NEO4J=neo4j:3.4.4-enterprise
+NEO4J=neo4j:3.5-enterprise
 
-docker run -d --name neo4j-empty --rm \
+docker run -d --name neo4j-empty \
+    --rm \
 	-p 127.0.0.1:7474:7474 \
-        -p 127.0.0.1:7687:7687 \
-        --env=NEO4J_dbms_memory_pagecache_size=1G \
-        --env=NEO4J_dbms_memory_heap_initial__size=2G \
-        --env=NEO4J_dbms_memory_heap_max__size=4G \
-	--env=NEO4J_AUTH=neo4j/admin \
-	--env=NEO4J_ACCEPT_LICENSE_AGREEMENT=yes \
-        -v $CWD/target:/plugins \
-        --env=NEO4J_dbms_security_procedures_unrestricted="com.maxdemarzi.*" \
+    -p 127.0.0.1:7687:7687 \
+    --env=NEO4J_dbms_memory_pagecache_size=1G \
+    --env=NEO4J_dbms_memory_heap_initial__size=2G \
+    --env=NEO4J_dbms_memory_heap_max__size=4G \
+    --env=NEO4J_AUTH=neo4j/admin \
+    --env=NEO4J_ACCEPT_LICENSE_AGREEMENT=yes \
+    -v $CWD/target:/var/lib/neo4j/plugins \
+    --env=NEO4J_dbms_security_procedures_unrestricted="com.maxdemarzi.*" \
 	-t $NEO4J
 
 echo "When you're ready to run some cypher, execute this:"
